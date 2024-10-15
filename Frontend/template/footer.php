44cd42abe924dd2b19549efdb4240e8d97f0ad5d
GUILHERME SANTOS SIQUEIRA <guilherme.ssiqueira4@senacsp.edu.br>
2024-10-15 20:18:44 -0300

    update mapao

--- Frontend/template/footer.php
+++ Frontend/template/footer.php
@@ -3,16 +3,6 @@
 </footer>
 <script src="assets/js/mdb.umd.min.js"></script>
 
-<script>
-  const myModal = document.getElementById('myModal')
-  const myInput = document.getElementById('myInput')
-
-  myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-  })
-</script>
-
-
 </body>
 
 </html>
